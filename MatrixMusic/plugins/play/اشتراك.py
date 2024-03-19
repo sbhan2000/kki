ab5ee6ebf344c3ba866f05aa614e4441c940bb19
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 19:59:45 +0300

    Update اشتراك.py

--- MatrixMusic/plugins/play/اشتراك.py
+++ MatrixMusic/plugins/play/اشتراك.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
 from pyrogram.enums import ChatType
 from pyrogram.errors import UserNotParticipant
-from ZeMusic import app
+from MatrixMusic import app
 
 
 async def subscription(_, __: Client, message: Message):
