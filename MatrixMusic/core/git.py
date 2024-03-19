c849c2f59b704035c6445ee8929cecd32ff13868
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 11:49:07 +0300

    Update git.py

--- MatrixMusic/core/git.py
+++ MatrixMusic/core/git.py
@@ -72,4 +72,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER("ميــوزك ريڤو").info(f"جارِ الكشف عن تحديثات جديدة . . .")
+        LOGGER("سورس حمد").info(f"جارِ الكشف عن تحديثات جديدة . . .")
