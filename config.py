76dc8134006a19bbc9764a2bfab95f783251c729
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 18:02:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -45,6 +45,8 @@ GIT_TOKEN = getenv(
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ah07v")
+MUST_JOIN = getenv("MUST_JOIN", "")
+
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
