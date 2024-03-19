a72a9faf68a8332b7b77e3b004ed2faff3ea5a10
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 13:01:08 +0300

    Update Devs.py

--- MatrixMusic/plugins/play/Devs.py
+++ MatrixMusic/plugins/play/Devs.py
@@ -33,15 +33,15 @@ def get_file_id(msg: Message):
                 return obj
 
 @app.on_message(
-    command(["إعدام","اعدام","المطور","ريڤو","ريفو","مطور السورس","مبرمج السورس"])
+    command(["المبرمج","حمد","مبرمج السورس"])
     & filters.group
   
 )
 async def yas(client, message):
-    usr = await client.get_chat("DAD_E3DAM")
+    usr = await client.get_chat("ah'2_v")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
+    await message.reply_photo(photo,       caption=f"**المبرمج\n🚦𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n🚦𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n🚦𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n🚦𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘",  
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -52,71 +52,3 @@ async def yas(client, message):
         ),
     )
     
-@app.on_message(
-    command(["إعدام","اعدام","المطور","ريڤو","ريفو","مطور السورس","مبرمج السورس"])
-    & filters.group
-  
-)
-async def yas(client, message):
-    usr = await client.get_chat("DAD_E3DAM")
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
-                ],
-            ]
-        ),
-    )
-    
-@app.on_message(
-    command(["مطورين","مطورين السورس","المطورين","سورس","السورس"])
-  
-)
-async def huhh(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://www7.0zz0.com/2024/02/28/05/415560079.jpg",
-        caption=f"""↯︙اهلا بك عزيزي {message.from_user.mention}\n↯︙مطورين سورس فينوم ميوزك""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "‹ : 𝗗𝗘𝗩 𝐄3𝐃𝐀𝐌 : ›", url=f"https://t.me/DAD_E3DAM"), 
-                 ],[
-                    InlineKeyboardButton(
-                        "‹ : 𝗗𝗘𝗩 𝐊𝐄𝐌𝐎 : ›", url=f"https://t.me/PG_7U"),
-                ],[
-                    InlineKeyboardButton(
-                        "‹ : 𓏺𝗦𝗼𝘂𝗿𝗰𝗲ᯓ𝗥𝗘𝗩𝗢𖠛 : ›", url=f"https://t.me/SOURCE_REVO"),
-                ],
-
-            ]
-
-        ),
-
-    )
-
-@app.on_message(command(["تخ"]) & filters.group)
-async def huhh(client, message):
-    to_id = int(ahmed.split("to")[-1].split("in")[0])
-    from_id = int(ahmed.split("ahmed")[-1].split("to")[0])
-    in_id = int(caption.split("in")[-1])
-    to_url = f"tg://openmessage?user_id={to_id}"
-    from_url = f"tg://openmessage?user_id={from_id}"
-    ahmed = message.text
-    await message.reply_animation(
-        animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
-        caption=f"""↯︙قتل ↫ ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\nالضحيه دا 😢 ↫ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
-    )
-    reply_markup=InlineKeyboardMarkup(
-
-       [
-           [
-               InlineKeyboardButton(
-                   "‹ : 𝐄3𝐃𝐀𝐌 𝖳𝖾𝖠𝗆 : ›", url=f"https://t.me/SOURCE_REVO"),
-           ],
-       ]
-    ),
