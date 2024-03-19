1db7056ace82ccb68239ac7e02fcac57d6dbf931
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 13:55:59 +0300

    Update playmode.py

--- MatrixMusic/plugins/play/playmode.py
+++ MatrixMusic/plugins/play/playmode.py
@@ -9,7 +9,7 @@ from MatrixMusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
-@app.on_message(command(["الاعدادات", "وضع شغل"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["وضع التشغيل", "وضع شغل"]) & filters.group & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
