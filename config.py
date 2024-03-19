0ffa6f9e3934667e4e585524545bf7430fb6cef9
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-19 10:53:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Darksourcebot/REVO_MUSIC",
+    "https://github.com/qwert200/bote",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
