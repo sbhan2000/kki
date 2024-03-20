39f8b5375d34905b0d6a7566617b281487c5e9e2
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-20 22:43:01 +0300

    Update المالك.py

--- MatrixMusic/plugins/play/المالك.py
+++ MatrixMusic/plugins/play/المالك.py
@@ -6,36 +6,31 @@ import aiohttp
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from strings.filters import command
+from MatrixMusic.filters import command
 from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from MatrixMusic import app
 from asyncio import gather
-from pyrogram import enums
-from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import FloodWait
 
 
-    
-@app.on_message(command(["المالك"]) & filters.group)
-async def creator(c, msg):
-    x = []
-    async for m in app.get_chat_members(msg.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        if m.status == ChatMemberStatus.OWNER:
-            adox = await app.get_chat(chat_id=m.user.id)
-            bio = adox.bio
-            if adox.photo:
-                async for photo in app.get_chat_photos(m.user.id, limit=1):
-                    await msg.reply_photo(
-                        photo.file_id,
-                        caption=f"᥆ꪝᥒ꧖ᖇ | - {adox.mention_markdown} ↯︙\n\nႦᎥ᥆ | - {adox.bio} ↯︙",
-                        reply_markup=InlineKeyboardMarkup(
-                            [[InlineKeyboardButton(adox.first_name, url=f"https://t.me/{adox.username}")]])
-                    )
-            else:
-                await msg.reply_text(
-                    f"᥆ꪝᥒ꧖ᖇ | - {adox.mention_markdown} ↯︙\n\nႦᎥ᥆ | - {adox.bio} ↯︙",
-                    reply_markup=InlineKeyboardMarkup(
-                        [[InlineKeyboardButton(adox.first_name, url=f"https://t.me/{adox.username}")]]
-                    )
-                )
-    else:
-        await msg.reply_text("الحساب محذوف")
+
+
+@app.on_message(command(["المالك"]) & filters.group )
+async def gak_owne(client: Client, message: Message):
+      if len(message.command) >= 2:
+         return 
+      else:
+            chat_id = message.chat.id
+            f = "administrators"
+            async for member in client.iter_chat_members(chat_id, filter=f):
+               if member.status == "creator":
+                 id = member.user.id
+                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
+                 m = await client.get_chat(id)
+                 if m.photo:
+                       photo = await app.download_media(m.photo.big_file_id)
+                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :`{m.id}`\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
+                 else:
+                    return await message.reply("• " + member.user.mention)
+                    
+                    
