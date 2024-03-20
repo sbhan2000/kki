cffd993b77a0aea03d95baac1a3389986bad7ff1
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-20 16:21:21 +0300

    Update اشتراك.py

--- MatrixMusic/plugins/play/اشتراك.py
+++ MatrixMusic/plugins/play/اشتراك.py
@@ -15,6 +15,7 @@ async def subscription(_, __: Client, message: Message):
     
 subscribed = filters.create(subscription)
 
+@app.on_message( filters.incoming & filters.private, group=-1)
 @app.on_message(~subscribed)
 async def checker(_: Client, message: Message):
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
@@ -31,33 +32,3 @@ async def checker(_: Client, message: Message):
 
 
 
-MUST_JOIN = "ah07v"
-#------------------------
-@app.on_message( filters.incoming & filters.private, group=-1)
-async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
-        return
-    try:
-        try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await app.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(f"عذراً عزيزي {user} عليك الإشتراك بقناة البوت اولا.",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("قناة البوت", url=link),
-                            ]
-                        ]
-                    )
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"ارفع البوت مشࢪف في القناة: {MUST_JOIN} !")
