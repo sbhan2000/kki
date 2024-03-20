6e3e185dd280c3424b818a29989709c52a5caffc
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-20 21:18:13 +0300

    Update قران.py

--- MatrixMusic/plugins/play/قران.py
+++ MatrixMusic/plugins/play/قران.py
@@ -5,9 +5,18 @@ import json
 from config import BANNED_USERS
 import requests 
 from MatrixMusic.core.call import Zelzaly
+import asyncio
+import os
+import time
+import requests
+from config import START_IMG_URL
+import random
+from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from MatrixMusic import app
+from random import  choice, randint
 
 
-@app.on_message(filters.command(["قران"],""))
+@app.on_message(filters.command(["المصحف"],""))
 async def quran(c,msg):
     quran = json.loads(open("MatrixMusic/assets/quran.json").read())["s"]
     keyboard = []
@@ -21,7 +30,9 @@ async def quran(c,msg):
         list.append(ikb(name, callback_data = f"play-{i-1}"))
     keyboard.append(list)
     keyboard.append([ikb(". التالي .", callback_data = "next-1")])
-    await msg.reply("• اهلا بك عزيزي قم باختيار السورة بالاذرار من اسفل .", reply_markup = ikm(keyboard))
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/14c7948ad180050fe16e4.jpg",
+        caption=f"""↯︙اهلا بك في قائمة اسماء قراء القران الكريم.""", reply_markup = ikm(keyboard))
 
 @app.on_callback_query(filters.regex("next") & ~BANNED_USERS)
 async def next_quran(c,cq):
@@ -39,7 +50,7 @@ async def next_quran(c,cq):
             name = quran[(i+(number*10))-1]["surah"]
             list.append(ikb(name, callback_data = f"play-{(i+(number*10))-1}"))
         keyboard.append(list)
-        keyboard.append([ikb(". السابق .", callback_data = f"next-{number-1}")])
+        keyboard.append([ikb("‹ رجوع ›", callback_data = f"next-{number-1}")])
     elif number == 0:
         for i in range(1,11):
             if len(list) == 2:
@@ -49,7 +60,7 @@ async def next_quran(c,cq):
             name = quran[i-1]["surah"]
             list.append(ikb(name, callback_data = f"play-{i-1}"))
         keyboard.append(list)
-        keyboard.append([ikb(". التالي .", callback_data = "next-1")])
+        keyboard.append([ikb("‹ التالي ›", callback_data = "next-1")])
     else:
         for i in range(1,11):
             if len(list) == 2:
@@ -59,7 +70,7 @@ async def next_quran(c,cq):
             name = quran[(i+(number*10))-1]["surah"]
             list.append(ikb(name, callback_data = f"play-{(i+(number*10))-1}"))
         keyboard.append(list)
-        keyboard.append([ikb(". السابق .", callback_data = f"next-{number-1}"), ikb(". التالي .", callback_data = f"next-{number+1}")])
+        keyboard.append([ikb("‹ رجوع ›", callback_data = f"next-{number-1}"), ikb(". التالي .", callback_data = f"next-{number+1}")])
     await cq.edit_message_reply_markup(ikm(keyboard))
             
 @app.on_callback_query(filters.regex("play") & ~BANNED_USERS)
@@ -78,13 +89,13 @@ async def show_quran(c,cq):
         list.append(ikb(name, callback_data = f"runq-{number}-{i}"))
     keyboard.append(list)
     if number == 114:
-        keyboard.append([ikb(". السابق .", callback_data = f"play-{number-1}")])
+        keyboard.append([ikb("‹ رجوع ›", callback_data = f"play-{number-1}")])
     elif number == 0:
-        keyboard.append([ikb(". التالي .", callback_data = f"play-{number+1}")])
+        keyboard.append([ikb("‹ التالي ›", callback_data = f"play-{number+1}")])
     else:
-        keyboard.append([ikb(". السابق .", callback_data = f"play-{number-1}"), ikb(". التالي .", callback_data = f"play-{number+1}")])
+        keyboard.append([ikb("‹ رجوع ›", callback_data = f"play-{number-1}"), ikb("‹ التالي ›", callback_data = f"play-{number+1}")])
     name_suarh = quran[number]["surah"]
-    await cq.edit_message_text(f"- تم اختيار سورة {name_suarh} .\n- قم باختيار الشيخ", reply_markup = ikm(keyboard))
+    await cq.edit_message_text(f"↯︙تم اختيار سورة {name_suarh} .\n↯︙قم باختيار الشيخ", reply_markup = ikm(keyboard))
 
 @app.on_callback_query(filters.regex("runq") & ~BANNED_USERS)
 async def show_quran(c,cq):
@@ -96,7 +107,7 @@ async def show_quran(c,cq):
     per_name = quran[number]["sounds"][i]["name"]
     file = requests.get(quran[number]["sounds"][i]["url"]).content
     open(f"{name}.mp3","wb").write(file)
-    await app.send_audio(cq.message.chat.id,f"{name}.mp3",f"- سورة {name} بصوت الشيخ {per_name} ",reply_markup = ikm([[ikb(". تشغيل .", callback_data=f"done-{number}-{i}")]]))
+    await app.send_audio(cq.message.chat.id,f"{name}.mp3",f"↯︙سورة {name} بصوت الشيخ {per_name} ",reply_markup = ikm([[ikb("‹ تشغيل ›", callback_data=f"done-{number}-{i}")]]))
 
 
 @app.on_callback_query(filters.regex("done") & ~BANNED_USERS)
@@ -107,7 +118,7 @@ async def show_quran(c,cq):
     quran = json.loads(open("MatrixMusic/assets/quran.json").read())["s"]
     try:
         await Zelzaly.join_call(cq.message.chat.id, cq.message.chat.id, quran[number]["sounds"][i]["url"], video=None)
-        await cq.edit_message_reply_markup(ikm([[ikb(". ايقاف مؤقت .", callback_data = f"ADMIN Pause|{cq.message.chat.id}"),ikb(". استئناف .", callback_data=f"ADMIN Resume|{cq.message.chat.id}")],[ikb(". ايقاف .", callback_data = f"ADMIN Stop|{cq.message.chat.id}")]]))
+        await cq.edit_message_reply_markup(ikm([[ikb("‹ ايقاف مؤقت ›", callback_data = f"ADMIN Pause|{cq.message.chat.id}"),ikb("‹ استئناف ›", callback_data=f"ADMIN Resume|{cq.message.chat.id}")],[ikb("‹ ايقاف ›", callback_data = f"ADMIN Stop|{cq.message.chat.id}")]]))
     except:
         await Zelzaly.skip_stream(cq.message.chat.id, quran[number]["sounds"][i]["url"])
-        await cq.edit_message_reply_markup(ikm([[ikb(". ايقاف مؤقت .", callback_data = f"ADMIN Pause|{cq.message.chat.id}"),ikb(". استئناف .", callback_data=f"ADMIN Resume|{cq.message.chat.id}")],[ikb(". ايقاف .", callback_data = f"ADMIN Stop|{cq.message.chat.id}")]]))
+        await cq.edit_message_reply_markup(ikm([[ikb("‹ ايقاف مؤقت ›", callback_data = f"ADMIN Pause|{cq.message.chat.id}"),ikb("‹ استئناف ›", callback_data=f"ADMIN Resume|{cq.message.chat.id}")],[ikb("‹ ايقاف ›", callback_data = f"ADMIN Stop|{cq.message.chat.id}")]]))
