14c0d17e4f7491e47bbda910e6117a12954403ce
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-20 23:48:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -105,21 +105,21 @@ autoclean = []
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
+    "START_IMG_URL", "https://te.legra.ph/file/fba5fe3667bf01a79376e.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
+    "PING_IMG_URL", "https://te.legra.ph/file/f5163a39051b0042b1181.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-STATS_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/db83c3505d2e7a9276cfe.png"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/9a5431c04152f6ee857e7.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/28c494c9e66a72444b49c.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/95919cc88e8d9fdcfc4ab.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ab4ebeacd32e8e9ad7ddf.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/132ad6f8204f2a2ca789f.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/69614571a91b77758e344.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
 def time_to_seconds(time):
