d7dbdd87c7a4881b8daa8b841626d92da4f717ba
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-21 23:21:33 +0300

    Update filters.py

--- strings/filters.py
+++ strings/filters.py
@@ -1,6 +1,9 @@
 from typing import List, Union
 
 from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import UserNotParticipant
 
 
 other_filters = filters.group  & ~filters.via_bot & ~filters.forwarded
@@ -11,3 +14,14 @@ other_filters2 = (
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
+
+
+
+#channel = "BENfiles" # Your Channel
+async def subscription(_, __: Client, message: Message):
+    user_id = message.from_user.id
+    try: await app.get_chat_member(channel, user_id)
+    except UserNotParticipant: return False
+    return True
+    
+subscribed = filters.create(subscription)
