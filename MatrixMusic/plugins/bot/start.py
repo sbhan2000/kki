559f9f42835f0b0d0a6bdcbb3aab4a43f6beccfb
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-22 14:13:14 +0300

    Update start.py

--- MatrixMusic/plugins/bot/start.py
+++ MatrixMusic/plugins/bot/start.py
@@ -32,7 +32,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_text(f"صلي علي النبي وتبسم ♥️✨")
+            await message.reply_text("صلي علي النبي وتبسم ♥️✨")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHANNEL),
