b3e4a2456a5d173f46b0d16183ef209756faad83
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-23 13:33:35 +0300

    Create المطور.py

---
+++ MatrixMusic/plugins/play/المطور.py
@@ -0,0 +1,33 @@
+import asyncio
+import os
+import requests
+import pyrogram
+from pyrogram import Client, filters, emoji
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+from ZeMusic import app
+from config import OWNER, LOGGER_ID
+
+
+@app.on_message(command(["مطور", "", "المطور"]))
+async def devid(client: Client, message: Message):
+    usr = await client.get_users(OWNER)
+    name = usr.first_name
+    usrnam = usr.username
+    uid = OWNER_ID
+    await app.download_media(usr.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
+       
+    await message.reply_photo(
+        photo="downloads/developer.jpg",
+        caption=f"""<b>↯︙𝙽𝙰𝙼𝙴 :</b> <a href='tg://user?id={uid}'>{name}</a>\n\n<b>↯︙𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> @{usrnam}""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(name, url=f"tg://user?id={uid}"),
+                ],[
+                    InlineKeyboardButton(text="『 السورس 』", url=config.SUPPORT_CHANNEL),
+                ],
+            ]
+        ),
+    )
