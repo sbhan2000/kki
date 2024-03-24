7a95ead42163f83aff80e7cdf15085d50993dde3
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-24 07:29:34 +0300

    Update المطور.py

--- MatrixMusic/plugins/play/المطور.py
+++ MatrixMusic/plugins/play/المطور.py
@@ -27,7 +27,7 @@ async def devid(client: Client, message: Message):
                     InlineKeyboardButton(name, url=f"tg://user?id={uid}"),
                 ],[
                     InlineKeyboardButton(
-                        "•✯  السورس  ✯•", url="https://t.me/EF_19"),
+                        "قناة الـبوت", url=config.SUPPORT_CHANNEL"),
                 ],
             ]
         ),
