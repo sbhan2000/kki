0da66976792d2fb4000cc94d2bf925cbf63946df
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-10 02:36:44 +0300

    Update skip.py

--- MatrixMusic/plugins/admins/skip.py
+++ MatrixMusic/plugins/admins/skip.py
@@ -116,7 +116,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Zelzaly.skip_stream(chat_id, link, video=status, image=image)
         except:
             return await message.reply_text(_["call_6"])
-        button = stream_markup(_, chat_id)
+        button = stream_markup(_, videoid, chat_id)
         img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
@@ -149,7 +149,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Zelzaly.skip_stream(chat_id, file_path, video=status, image=image)
         except:
             return await mystic.edit_text(_["call_6"])
-        button = stream_markup(_, chat_id)
+        button = stream_markup(_, videoid, chat_id)
         img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
@@ -169,7 +169,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Zelzaly.skip_stream(chat_id, videoid, video=status)
         except:
             return await message.reply_text(_["call_6"])
-        button = stream_markup(_, chat_id)
+        button = stream_markup(_, videoid, chat_id)
         run = await message.reply_photo(
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
@@ -192,7 +192,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await message.reply_text(_["call_6"])
         if videoid == "telegram":
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, videoid, chat_id)
             run = await message.reply_photo(
                 photo=config.TELEGRAM_AUDIO_URL
                 if str(streamtype) == "audio"
@@ -205,7 +205,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
         elif videoid == "soundcloud":
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, videoid, chat_id)
             run = await message.reply_photo(
                 photo=config.SOUNCLOUD_IMG_URL
                 if str(streamtype) == "audio"
@@ -218,7 +218,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
         else:
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, videoid, chat_id)
             img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
